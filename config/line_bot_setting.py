0b81144c40be710d011914d49c90391ed9bd7680
KoalaAcademy <koala.python.academy@gmail.com>
2025-10-04 15:05:48 +0800

    Update line_bot_setting.py

--- config/line_bot_setting.py
+++ config/line_bot_setting.py
@@ -1,6 +1,7 @@
 CHANNEL_ACCESS_TOKEN = 'ry9jEoZkaan4jXdAVK1KaLgS7BkdiUEDWRhT2NeU09+lzWpW1lbBQ/xhVEskXSNiTdYtF1mDEjwP4RQhn/v/pH3lFoVgwHo5BIoTd3wnJqZptvJWx5SeKs7NCVoZ+qYTZwz2NzBPUvwaSOKFePcM6QdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET = '940f0961ea6aee2849c743612ce8f7f2'
-BASE_URL = 'https://2112f7cc0c39.ngrok-free.app'
+BASE_URL = 'https://skillflow1004-2.onrender.com'
 # https://skill-flow-seven.vercel.app/
+
 
 
