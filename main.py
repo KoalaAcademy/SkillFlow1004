2dc4fa7ff6b8222d996504c1b0f0b3b6b9eccebe
KoalaAcademy <koala.python.academy@gmail.com>
2025-10-04 14:45:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def callback():
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Render 會提供 PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+
+
 
 
